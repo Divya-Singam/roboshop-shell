bf4ead0303b1467d825299c3cf0115c07ebb9384
Divya <sreedivya1206@gmail.com>
2023-06-15 23:45:05 +1000

    Second shell script

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[34mcopy mongodb repo file \e[0m"
-cp mongodb.repo/etc/yum.repos.d/mongo.repo
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 echo -e "\e[34minstalling mongodb repo file \e[0m"
 yum install mongodb-org -y
