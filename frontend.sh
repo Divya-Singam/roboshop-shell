ee0d2a131fec1689b66ab4303fab4127c17e257f
Divya <sreedivya1206@gmail.com>
2023-06-15 23:13:54 +1000

    Second shell script

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
-yum install nginx -y
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+yum install nginx -y  >/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* >/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip  >/tmp/roboshop.log
 #we need to copy config file
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx  >/tmp/roboshop.log
+systemctl restart nginx  >/tmp/roboshop.log
