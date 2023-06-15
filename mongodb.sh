8daf6c091b00c0e7ed120183b0b37913018ce14c
Divya <sreedivya1206@gmail.com>
2023-06-15 23:43:05 +1000

    Second shell script

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
-echo -e "\e[33mcopy mongodb repo file \e[0m"
-cp mongodb.repo//etc/yum.repos.d/mongo.repo
+echo -e "\e[34mcopy mongodb repo file \e[0m"
+cp mongodb.repo/etc/yum.repos.d/mongo.repo
 
-echo -e "\e[33installing mongodb repo file \e[0m"
+echo -e "\e[34minstalling mongodb repo file \e[0m"
 yum install mongodb-org -y
 
 #modify the config file
 
-echo -e "\e[33start mongodb repo file \e[0m"
+echo -e "\e[34mstart mongodb repo file \e[0m"
 systemctl enable mongod
 systemctl restart mongod
