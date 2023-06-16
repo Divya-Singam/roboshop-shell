c2066ce29a539dbab7410a9cd282bb89dce3cf69
Divya <sreedivya1206@gmail.com>
2023-06-16 22:50:09 +1000

    mongodb.repo

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,11 @@
 echo -e "\e[34mcopy mongodb repo file \e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 
-#echo -e "\e[34minstalling Mongodb server \e[0m"
-#yum install mongodb-org -y
+echo -e "\e[34minstalling Mongodb server \e[0m"
+yum install mongodb-org -y
 
 #modify the config file
 
-#echo -e "\e[34mstart Mongodb servive \e[0m"
-#systemctl enable mongod
-#systemctl restart mongod
+echo -e "\e[34mstart Mongodb servive \e[0m"
+systemctl enable mongod
+systemctl restart mongod
